a3c61f97ddd9ad9e458ee3dc2acb41e15dcbac35
Diogo Nunes <dialex.nunes@gmail.com>
2017-06-15 10:37:07 +0100

    FIX: successful read from targets.json config file

--- src/main/java/Fetcher.java
+++ src/main/java/Fetcher.java
@@ -1,6 +1,7 @@
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -8,9 +9,13 @@ public class Fetcher {
 
     public static void main(String[] args) {
         try {
-            Config targets = readConfig();
-            Object results = fetchData(targets);
-            writeResults(results);
+            Config config = readConfig();
+            if (config.targets.size() > 0) {
+                Object results = fetchData(config.targets);
+                writeResults(results);
+            } else {
+                System.out.println("No tasks found on config file. Shutting down...");
+            }
         } catch (Exception e) {
             System.out.println("The program will exit due to a fatal error: " + e.getMessage());
             System.out.println("\nDebug details: " + e.getStackTrace());
@@ -28,6 +33,7 @@ public class Fetcher {
     private static Config readConfig() throws IOException {
         String CONFIG_FILENAME = "targets.json";
         try {
+            System.out.println("Looking for file " + System.getProperty("user.dir") + File.separator + CONFIG_FILENAME);
             JsonReader reader = new JsonReader(new FileReader(CONFIG_FILENAME));
             return new Gson().fromJson(reader, Config.class);
         }
